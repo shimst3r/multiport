972a1688ebbd2ecc8bc4db5a3b0d6e86259ff48e
Nils Müller <shimst3r@gmail.com>
2020-09-21 11:40:13 +0200

    Add __del__ and cleanup for connections

--- multiport/scanner.py
+++ multiport/scanner.py
@@ -21,14 +21,23 @@ class Scanner:
         self.host = host
         self.open_ports = []
         self.socket = socket.socket()
+        logging.debug(f"Socket at host {self.host} has been created.")
+
+    def __del__(self):
+        self.socket.close()
+        logging.debug(f"Socket at host {self.host} has been closed.")
 
     def scan(self, port: int):
         """Scans whether the given port is open."""
         try:
+            logging.debug(f"Trying to connect to {self.host}:{port}.")
             self.socket.connect((self.host, port))
             self.open_ports.append(port)
-            logging.debug(f"Port {port} is open.")
+            logging.debug(f"Port {self.host}:{port} is open.")
         except ConnectionRefusedError:
-            logging.debug(f"Port {port} is closed.")
+            logging.debug(f"Port {self.host}:{port} is closed.")
         except OSError as os_error:
             logging.debug(os_error)
+        else:
+            self.socket.shutdown(socket.SHUT_RDWR)
+            logging.debug(f"Connection to port {self.host}:{port} has been shut down.")
